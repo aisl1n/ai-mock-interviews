e793dce6fb5ce14e9b8ab9ef0c3f02a15fc5b5e6
aisl1n <aislanarchy@gmail.com>
2025-04-22 20:50:43 -0300

    fix: adjust local next public keys for firebase admin

--- firebase/admin.ts
+++ firebase/admin.ts
@@ -1,6 +1,6 @@
-import { cert, getApps, initializeApp } from 'firebase-admin/app';
-import { getAuth } from 'firebase-admin/auth';
-import { getFirestore } from 'firebase-admin/firestore';
+import { cert, getApps, initializeApp } from "firebase-admin/app";
+import { getAuth } from "firebase-admin/auth";
+import { getFirestore } from "firebase-admin/firestore";
 
 const initFirebaseAdmin = () => {
   const apps = getApps();
@@ -8,9 +8,12 @@ const initFirebaseAdmin = () => {
   if (!apps.length) {
     initializeApp({
       credential: cert({
-        projectId: process.env.FIREBASE_PROJECT_ID,
-        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+        clientEmail: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL,
+        privateKey: process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY?.replace(
+          /\\n/g,
+          "\n"
+        ),
       }),
     });
   }
