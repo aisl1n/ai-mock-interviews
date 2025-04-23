f1941fb12a73a41ad21ef448c861f8fb6db5c0b1
aisl1n <aislanarchy@gmail.com>
2025-04-22 21:16:04 -0300

    fix: remove console log

--- firebase/client.ts
+++ firebase/client.ts
@@ -2,9 +2,6 @@ import { initializeApp, getApp, getApps } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
-console.log("aqui");
-console.log(process.env.NEXT_PUBLIC_FIREBASE_API_KEY);
-
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
